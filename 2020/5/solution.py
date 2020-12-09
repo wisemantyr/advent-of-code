160348baafc266b07992e3fdcf5147f145c705ee
unknown <wisemantyr@gmail.com>
2020-12-08 23:56:55 -0500

    starting day 7

--- 2020/5/solution.py
+++ 2020/5/solution.py
@@ -59,17 +59,18 @@ for seat in inp:
     seat_info.setdefault('seat', (row, col))
     seats.append(seat_info)
 
-highest_id = max(seat['id'] for seat in seats)
-print(f"Part 1 Solution: {highest_id}")
 
 seat_ids = [seat['id'] for seat in seats]
+highest_id = max(seat_ids)
+print(f"Part 1 Solution: {highest_id}")
+
+
 assigned = [seat['seat'] for seat in seats]
 rows = [x for x,y in assigned]
 cols = [y for x,y in assigned]
 
 data = {'id': seat_ids, 'row': rows, 'col': cols}
 seats_df = pd.DataFrame.from_dict(data)
-seats_df = seats_df.set_index('id')
 
 high_row = seats_df['row'].max()
 low_row = seats_df['row'].min()
@@ -77,21 +78,21 @@ low_row = seats_df['row'].min()
 seats_df = seats_df.loc[(seats_df['row'] != high_row) & (seats_df['row'] != low_row)]
 row_groups = seats_df.groupby('row').size()
 rows_open = row_groups[row_groups < 8]
+
 row_nums_open = rows_open.index.to_list()
+all_col_nums = [num for num in range(first_col, last_col + 1)]
+
+all_nearby = []
+for row in row_nums_open:
+    for col in all_col_nums:
+        seat = (row,col)
+        all_nearby.append(seat)
 
 rows_open_df = seats_df.loc[seats_df['row'].isin(row_nums_open)]
 open_seats_rows = rows_open_df['row'].to_list()
 open_seats_cols = rows_open_df['col'].to_list()
-
 seated_nearby = list(zip(open_seats_rows, open_seats_cols))
 
-col_nums = [num for num in range(first_col, last_col + 1)]
-
-all_nearby = []
-for row in row_nums_open:
-    for col in col_nums:
-        seat = (row,col)
-        all_nearby.append(seat)
 
 missing_seats = [seat for seat in seated_nearby + all_nearby
                 if seat not in seated_nearby or seat not in all_nearby]
@@ -105,7 +106,6 @@ def check_surrounding(missing_seats, assigned):
         seat_down = (a[0] - 1, a[1])
         seat_left = (a[0], a[1] + 1)
         seat_right = (a[0], a[1] - 1)
-        
         check_seats = [seat_up, seat_down, seat_left, seat_right]
         
         for b in check_seats:
